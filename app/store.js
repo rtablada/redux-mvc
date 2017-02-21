78d03148601ee6694edf961b2f5bbe799198257e
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-20 22:18:43 -0600

    Move reducer to module

--- app/store.js
+++ app/store.js
@@ -1,44 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import reducer from './reducer';
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  (state, { type, ...action }) => {
-    switch (type) {
-      case 'SONGS@FINDALL_STARTED':
-        return {
-          ...state,
-          fetching: true,
-        };
-      case 'SONGS@FINDALL_COMPLETE':
-        return {
-          ...state,
-          fetching: false,
-          songs: action.data,
-        };
-      case 'SONGS@CREATE_STARTED':
-        return {
-          ...state,
-          fetching: true,
-          songs: [...state.songs, action.data],
-        };
-      case 'SONGS@CREATE_SAVED':
-        return {
-          ...state,
-          fetching: false,
-          songs: state.songs.reduce((accum, curr) => {
-            if (curr._id && curr._id !== action.data._id) {
-              return [...accum, curr];
-            }
-
-            return [...accum, action.data];
-          }, []),
-        };
-      default:
-        return state;
-    }
-  },
+  reducer,
   { songs: [], fetching: false },
   composeEnhancers(
     applyMiddleware(thunk)
